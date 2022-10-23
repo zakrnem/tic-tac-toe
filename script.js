1231e7b97202737b5e15c184b88c0d976ea9eddc
zakrasty <zakrnem@zohomail.com>
2022-10-23 17:28:38 -0600

    Reset button working

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-(function () {
-    sum = 2;
-    gameGrid = document.querySelector('.game-grid')
-    gameGrid.addEventListener('click', (e) => logGrid(e));
-    return logGrid  = function (e) {
-        console.log(e.target.className)
+let gameTurn = 0;
+    
+gameGrid = document.querySelector('.game-grid')    
+gameGrid.addEventListener('click', (e) => markGrid(e))
+markGrid  = function (e) {
+    let target = gameGrid.querySelector(`.${e.target.className}`);
+    if (gameTurn === 0) {
+        target.textContent = 'X'
+        gameTurn += 1;
     }
-})()
+    else if (gameTurn === 1) {
+        target.textContent = 'O'
+        gameTurn -= 1;
+    }
+}
+
+resetButton = document.querySelector('#reset')
+resetButton.addEventListener('click', () => resetGrid())
+resetGrid = function () {
+    let gridArray = gameGrid.getElementsByTagName('div')
+    for (let i = gridArray.length-1; i>=0; i--) {
+        gridArray[i].textContent = ''
+    }
+}
+
     
 
 
