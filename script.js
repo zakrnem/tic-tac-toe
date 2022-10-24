a38e9821c1b619efa8b1f1263430f28d07c33247
zakrasty <zakrnem@zohomail.com>
2022-10-23 18:14:19 -0600

    Print game marks on array and resetting it working

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-let gameTurn = 0;
+let ticTacToe = (function () {
+    let gameGrid = document.querySelector('.game-grid')    
+    gameGrid.addEventListener('click', (e) => markGridDOM(e))
+    let gameTurn = 0
     
-gameGrid = document.querySelector('.game-grid')    
-gameGrid.addEventListener('click', (e) => markGrid(e))
-markGrid  = function (e) {
-    let target = gameGrid.querySelector(`.${e.target.className}`);
-    if (gameTurn === 0) {
-        target.textContent = 'X'
-        gameTurn += 1;
+    let markGridDOM  = function (e) {
+        let target = document.getElementById(`${e.target.id}`);
+        if (gameTurn === 0) {
+            target.textContent = 'X'
+            gameTurn += 1;
+            markGridArray(e, 'X')
+        }
+        else if (gameTurn === 1) {
+            target.textContent = 'O'
+            gameTurn -= 1;
+            markGridArray(e, 'O')
+        }
     }
-    else if (gameTurn === 1) {
-        target.textContent = 'O'
-        gameTurn -= 1;
-    }
-}
 
-resetButton = document.querySelector('#reset')
-resetButton.addEventListener('click', () => resetGrid())
-resetGrid = function () {
-    let gridArray = gameGrid.getElementsByTagName('div')
-    for (let i = gridArray.length-1; i>=0; i--) {
-        gridArray[i].textContent = ''
+    let gameGridArray = Array.from({length: 9})
+    let markGridArray = function (e, mark) {
+        gameGridArray[e.target.id] = mark;
+        console.log(gameGridArray)
     }
-}
+    
+    let resetButton = document.querySelector('#reset')
+    resetButton.addEventListener('click', () => resetGrid())
+    
+    let resetGrid = function () {
+        let gridArray = gameGrid.getElementsByTagName('div')
+        for (let i = gridArray.length-1; i>=0; i--) {
+            gridArray[i].textContent = ''
+            gameGridArray[i] = ''
+            console.log(gameGridArray)
+        }
+    }
+})()
+
+
 
     
 
