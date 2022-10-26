40eb7f8d237834f7dd19688689efbf0b1c62a59b
zakrasty <zakrnem@zohomail.com>
2022-10-25 20:02:08 -0600

    Object literal pattern working

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ let ticTacToe = {
     gameTurn: 0,
     
     //Grid event listener
-    gridListener: (function () {
-        let gameGrid = document.querySelector('.game-grid')
-        gameGrid.addEventListener('click', (e) => ticTacToe.markGrid(e))
-    })(),
+    gridListener: document.querySelector('.game-grid').addEventListener('click', (e) => ticTacToe.markGrid(e)),
+
     
     //Mark DOM & Array
     markGrid: function (e) {
@@ -17,92 +15,94 @@ let ticTacToe = {
             this.gameTurn += 1;
             this.gameCount += 1;
             this.markGridArray(e, 'X')
-            //gamePlay()
+            this.gamePlay()
         }
         else if (this.gameTurn === 1 && target.textContent === '') {
             target.textContent = 'O'
             this.gameTurn -= 1;
             this.gameCount += 1;
             this.markGridArray(e, 'O')
-            //gamePlay()
+            this.gamePlay()
         }
     },
 
     //Game array
     gameGridArray: Array.from({length: 9}),
-
     markGridArray: function (e, mark) {
         this.gameGridArray[e.target.id] = mark;
         console.log(this.gameGridArray)
-    }
+    },
 
-/*     //Result rules
-    let result = document.querySelector('.result')
-    function gamePlay() {
+    //Result rules
+    gamePlay: function() {
         switch(true) {
-            case (gameGridArray[0] === gameGridArray[1]
-                && gameGridArray[0] === gameGridArray [2]
-                && gameGridArray[0] !== undefined):
-                resultMessage();
+            case (this.gameGridArray[0] === this.gameGridArray[1]
+                && this.gameGridArray[0] === this.gameGridArray [2]
+                && this.gameGridArray[0] !== undefined):
+                this.resultMessage();
                 break;
-            case (gameGridArray[3] === gameGridArray[4]
-                && gameGridArray[3] === gameGridArray [5]
-                && gameGridArray[3] !== undefined):
-                resultMessage();
+            case (this.gameGridArray[3] === this.gameGridArray[4]
+                && this.gameGridArray[3] === this.gameGridArray [5]
+                && this.gameGridArray[3] !== undefined):
+                this.resultMessage();
                 break;
-            case (gameGridArray[6] === gameGridArray[7]
-                && gameGridArray[6] === gameGridArray [8]
-                && gameGridArray[6] !== undefined):
-                resultMessage();
+            case (this.gameGridArray[6] === this.gameGridArray[7]
+                && this.gameGridArray[6] === this.gameGridArray [8]
+                && this.gameGridArray[6] !== undefined):
+                this.resultMessage();
                 break;
-            case (gameGridArray[0] === gameGridArray[3]
-                && gameGridArray[0] === gameGridArray [6]
-                && gameGridArray[0] !== undefined):
-                resultMessage();
+            case (this.gameGridArray[0] === this.gameGridArray[3]
+                && this.gameGridArray[0] === this.gameGridArray [6]
+                && this.gameGridArray[0] !== undefined):
+                this.resultMessage();
                 break;
-            case (gameGridArray[1] === gameGridArray[4]
-                && gameGridArray[1] === gameGridArray [7]
-                && gameGridArray[1] !== undefined):
-                resultMessage();
+            case (this.gameGridArray[1] === this.gameGridArray[4]
+                && this.gameGridArray[1] === this.gameGridArray [7]
+                && this.gameGridArray[1] !== undefined):
+                this.resultMessage();
                 break;
-            case (gameGridArray[2] === gameGridArray[5]
-                && gameGridArray[2] === gameGridArray [8]
-                && gameGridArray[2] !== undefined):
-                resultMessage();
+            case (this.gameGridArray[2] === this.gameGridArray[5]
+                && this.gameGridArray[2] === this.gameGridArray [8]
+                && this.gameGridArray[2] !== undefined):
+                this.resultMessage();
                 break;
-            case (gameGridArray[0] === gameGridArray[4]
-                && gameGridArray[0] === gameGridArray [8]
-                && gameGridArray[0] !== undefined):
-                resultMessage();
+            case (this.gameGridArray[0] === this.gameGridArray[4]
+                && this.gameGridArray[0] === this.gameGridArray [8]
+                && this.gameGridArray[0] !== undefined):
+                this.resultMessage();
                 break;
-            case (gameGridArray[2] === gameGridArray[4]
-                && gameGridArray[2] === gameGridArray [6]
-                && gameGridArray[2] !== undefined):
-                resultMessage();
+            case (this.gameGridArray[2] === this.gameGridArray[4]
+                && this.gameGridArray[2] === this.gameGridArray [6]
+                && this.gameGridArray[2] !== undefined):
+                this.resultMessage();
                 break;
-            case (gameCount === 9 && result.textContent === ''):
-                result.textContent = `It's a tie`;
-        }
-        function resultMessage() {
-            gameTurn === 1 ? result.textContent = 'Player #1 won!'
-            : result.textContent = 'Player #2 won!'
-            gameTurn = undefined
-        }
-    }
+            case (this.gameCount === 9 && this.result.textContent === ''):
+                this.result.textContent = `It's a tie`;
+        }             
+    },
+
+    //Result message
+    result: document.querySelector('.result'),
+    resultMessage: function () {
+        this.gameTurn === 1 ? ticTacToe.result.textContent = 'Player #1 won!'
+        : ticTacToe.result.textContent = 'Player #2 won!'
+        this.gameTurn = undefined
+    },
     
     //Reset game
-    let resetButton = document.querySelector('#reset')
-    resetButton.addEventListener('click', () => resetGame())
-    
-    function resetGame() {
-        gameTurn = 0
-        gameCount = 0
-        result.textContent = ''
-        let gridArray = gameGrid.getElementsByTagName('div')
+    gameGrid: document.querySelector('.game-grid'),
+    resetListener: (function() {
+        let resetButton = document.querySelector('#reset')
+        resetButton.addEventListener('click', () => ticTacToe.resetGame())
+    })(),    
+    resetGame: function() {
+        this.gameTurn = 0
+        this.gameCount = 0
+        this.result.textContent = ''
+        let gridArray = this.gameGrid.getElementsByTagName('div')
         for (let i = gridArray.length-1; i>=0; i--) {
             gridArray[i].textContent = ''
-            gameGridArray[i] = undefined
+            this.gameGridArray[i] = undefined
         }
-        console.log(gameTurn)
-    } */
+    }
 }
